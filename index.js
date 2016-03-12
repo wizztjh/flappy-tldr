47847c5d6c5b06f92b3e5f69708a85cd34654833
Michal J. Gajda <mjgajda@gmail.com>
2016-03-13 06:33:21 +0800

    Make it work remotely.

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ function preload() {
 
     game.stage.backgroundColor = '#85b5e1';
 
-    game.load.baseURL = 'http://localhost:8000/assets/';
+    //game.load.baseURL = 'http://localhost:8000/assets/';
+    game.load.baseURL = 'https://rawgit.com/wizztjh/flappy-tldr/master/assets/';
     game.load.crossOrigin = 'anonymous';
 
     game.load.image('player', 'flappy.png');
